501d088449a6953f5687aab6a1e53774a4ddaafe
jameswawright <jameswawright@icloud.com>
2025-12-09 15:56:51 +0000

    Add player controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Initialize speed variable to control vehicle movement
-    public float speed = 15f;
+    private float speed = 15.0f;
+    // Initialize turnSpeed variable to control vehicle turning
+    private float turnSpeed = 25.0f; 
+    // Initialise horzontalInput and verticalInput variables to capture player input
+    private float horizontalInput;
+    private float verticalInput;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,7 +20,14 @@ public class PlayerController : MonoBehaviour
     // LateUpdate is called once per frame after all Update methods have been called
     void LateUpdate()
     {
+        // Capture horizontal and vertical input from player
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // Get horizontal input for turning by rotating around y axis via Vector3.up
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
